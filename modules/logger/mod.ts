ab349feec1320a0b21da900a96ceb7eacaa2ae0f
Michael Grenier <michael@grenier.dev>
2022-01-12 09:59:00 -0500

    fix: formatting

--- modules/logger/mod.ts
+++ modules/logger/mod.ts
@@ -1,23 +1,38 @@
-export type Logger = (namespace: string, level: string, message: string) => void;
+export type Logger = (
+	namespace: string,
+	level: string,
+	message: string,
+) => void;
 
-let globalLogger: Logger = (ns, lvl, msg) => { console.log(`${new Date().toISOString()} [${ns}] ${lvl} ${msg}`) };
+let globalLogger: Logger = (ns, lvl, msg) => {
+	console.log(`${new Date().toISOString()} [${ns}] ${lvl} ${msg}`);
+};
 
 export function createLogger(logger: Logger) {
 	globalLogger = logger;
 }
 
-
 export function logger(namespace: string) {
 	return {
-		debug: (message: string) => { globalLogger(namespace, 'debug', message) },
-		log: (message: string) => { globalLogger(namespace, 'debug', message) },
-		info: (message: string) => { globalLogger(namespace, 'debug', message) },
-		warn: (message: string) => { globalLogger(namespace, 'debug', message) },
-		error: (message: string) => { globalLogger(namespace, 'debug', message) },
-	}
+		debug: (message: string) => {
+			globalLogger(namespace, "debug", message);
+		},
+		log: (message: string) => {
+			globalLogger(namespace, "debug", message);
+		},
+		info: (message: string) => {
+			globalLogger(namespace, "debug", message);
+		},
+		warn: (message: string) => {
+			globalLogger(namespace, "debug", message);
+		},
+		error: (message: string) => {
+			globalLogger(namespace, "debug", message);
+		},
+	};
 }
 
-const defaultLogger = logger('default');
+const defaultLogger = logger("default");
 
 export const debug = defaultLogger.debug;
 export const log = defaultLogger.log;

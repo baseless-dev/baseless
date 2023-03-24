7ee0afc2d0c0dd19cef063ecec374a7e96b17b88
Michael Grenier <michael@grenier.dev>
2023-03-24 12:47:45 -0400

    chore: deno bundle

--- modules/baseless/cli/bundle.ts
+++ modules/baseless/cli/bundle.ts
@@ -1,6 +1,9 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.1/mod.ts";
 import * as esbuild from "https://deno.land/x/esbuild@v0.15.8/mod.js";
-import { dirname, extname, isAbsolute, join } from "https://deno.land/std@0.179.0/path/mod.ts";
+import { dirname, extname, isAbsolute, join, relative } from "https://deno.land/std@0.179.0/path/mod.ts";
+import * as ansi from "https://deno.land/x/ansi@1.0.1/mod.ts";
+import * as colors from "https://deno.land/std@0.165.0/fmt/colors.ts";
+import { prettyBytes } from "https://deno.land/x/pretty_bytes@v2.0.0/mod.ts";
 
 const build = new Command()
 	.name("Simple bundler")
@@ -76,7 +79,7 @@ const build = new Command()
 						response = cached;
 					} else {
 						response = await fetch(args.path);
-						// httpCache.put(args.path, response.clone());
+						httpCache.put(args.path, response.clone());
 					}
 					const contents = await response.text();
 					const ct = response.headers.get("Content-Type") ?? "text/javascript; charset=utf-8";
@@ -85,7 +88,9 @@ const build = new Command()
 			},
 		};
 
-		await esbuild.build({
+		const timeStart = performance.now();
+		console.log(`${colors.green(colors.bold(`PetiteVITE`) + ` v0.0.0`)} ${colors.blue("building for production...")}`);
+		const result = await esbuild.build({
 			entryPoints: [join(Deno.cwd(), options.entry)],
 			outdir: join(Deno.cwd(), options.dist),
 			bundle: true,
@@ -96,7 +101,45 @@ const build = new Command()
 			platform: "browser",
 			plugins: [BundleWebPlugin]
 		});
+		console.log(colors.green("✓") + colors.dim(` ${Object.keys(result.metafile!.inputs).length} modules transformed in ${(performance.now() - timeStart).toFixed(0)}ms.`));
+		const outouts = result.metafile!.outputs;
+		const sortedOutput = Object.entries(outouts);
+		sortedOutput.sort((a, b) => a[0].localeCompare(b[0]));
+		for await (const [path, meta] of sortedOutput) {
+			let color = colors.yellow;
+			const ext = extname(path);
+			if (ext === ".html") {
+				color = colors.green;
+			} else if (ext === ".css") {
+				color = colors.magenta;
+			} else if (ext === ".js") {
+				color = colors.blue;
+			}
+			const file = await Deno.open(path);
+			const stat = await file.stat();
+			const compressed = await new Response(file.readable.pipeThrough(new CompressionStream("gzip"))).arrayBuffer();
 
+			console.log(
+				color(path) +
+				colors.dim(` (${prettyBytes(stat.size)} ⇒ ${prettyBytes(compressed.byteLength)})`),
+			);
+
+			const sortedDeps = Object.entries(meta.inputs);
+			sortedDeps.sort((a, b) => b[1].bytesInOutput - a[1].bytesInOutput);
+
+			if (sortedDeps.length > 0) {
+				const moduleCount = sortedDeps.length;
+				let i = 0;
+				const biggestOffenders = sortedDeps.splice(0, 5);
+				for (const [dep, { bytesInOutput }] of biggestOffenders) {
+					console.log(colors.dim(`  ${++i >= moduleCount ? "└" : "├"} ${dep.replace("bundle-http:", "")} (${prettyBytes(bytesInOutput)})`));
+				}
+				if (sortedDeps.length) {
+					const depSize = prettyBytes(sortedDeps.reduce((size, dep) => size + dep[1].bytesInOutput, 0));
+					console.log(colors.dim(`  └ and ${colors.underline(`${sortedDeps.length} others modules`)} (${depSize})`));
+				}
+			}
+		}
 		Deno.exit(0);
 	});
 

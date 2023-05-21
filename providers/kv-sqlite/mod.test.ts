e76509aa2fde679a41a5a554212186130522d4ea
Michael Grenier <michael@grenier.dev>
2023-05-20 21:42:15 -0400

    refactor: removed kv-sqlite

--- providers/kv-sqlite/mod.test.ts
+++
@@ -1,9 +0,0 @@
-import testKVProvider from "../kv.test.ts";
-import { SqliteKVProvider } from "./mod.ts";
-
-Deno.test("SqliteKVProvider", async (t) => {
-	const kv = new SqliteKVProvider(":memory:");
-	await kv.open();
-	await testKVProvider(kv, t);
-	await kv.close();
-});

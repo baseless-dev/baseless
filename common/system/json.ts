7efc6c95e4279726225550614d3949988c214e9f
Michael Grenier <michael@grenier.dev>
2023-07-27 21:26:09 -0400

    chore(schema): remove type guard helpers from json

--- common/system/json.ts
+++ common/system/json.ts
@@ -1,47 +1,23 @@
-import { assertSchema, isSchema, type Schema } from "../schema/types.ts";
+import { type Schema } from "../schema/types.ts";
 import * as s from "../schema/schema.ts";
 
-export type Json =
-	| JsonValue
-	| JsonArray
-	| JsonRecord;
+export type Json = JsonValue | JsonArray | JsonRecord;
 export type JsonValue = string | number | boolean | null;
 export type JsonArray = Array<Json>;
 export type JsonRecord = { [key: string]: Json };
 
-const JsonValueSchema: Schema<JsonValue> = s.union([
+export const JsonValueSchema: Schema<JsonValue> = s.union([
 	s.string(),
 	s.number(),
 	s.boolean(),
 	s.nill(),
 ]);
-const JsonArraySchema: Schema<JsonArray> = s.lazy(() => s.array(JsonSchema));
-const JsonRecordSchema: Schema<JsonRecord> = s.lazy(() => s.record(JsonSchema));
-const JsonSchema: Schema<Json> = s.lazy(() =>
+export const JsonArraySchema: Schema<JsonArray> = s.lazy(() =>
+	s.array(JsonSchema)
+);
+export const JsonRecordSchema: Schema<JsonRecord> = s.lazy(() =>
+	s.record(JsonSchema)
+);
+export const JsonSchema: Schema<Json> = s.lazy(() =>
 	s.union([JsonValueSchema, JsonArraySchema, JsonRecordSchema])
 );
-
-export function isJsonValue(value: unknown): value is JsonValue {
-	return isSchema(JsonValueSchema, value);
-}
-export function isJsonArray(value: unknown): value is JsonArray {
-	return isSchema(JsonArraySchema, value);
-}
-export function isJsonRecord(value: unknown): value is JsonRecord {
-	return isSchema(JsonRecordSchema, value);
-}
-export function isJson(value: unknown): value is Json {
-	return isSchema(JsonSchema, value);
-}
-export function assertJsonValue(value: unknown): asserts value is JsonValue {
-	assertSchema(JsonValueSchema, value);
-}
-export function assertJsonArray(value: unknown): asserts value is JsonArray {
-	assertSchema(JsonArraySchema, value);
-}
-export function assertJsonRecord(value: unknown): asserts value is JsonRecord {
-	assertSchema(JsonRecordSchema, value);
-}
-export function assertJson(value: unknown): asserts value is Json {
-	assertSchema(JsonSchema, value);
-}
--- server/api/get_json_data.ts
+++ server/api/get_json_data.ts
@@ -1,10 +1,11 @@
-import { assertJsonRecord, type JsonRecord } from "../../common/system/json.ts";
+import { assertSchema } from "../../common/schema/types.ts";
+import { type JsonRecord, JsonRecordSchema } from "../../common/system/json.ts";
 
 export async function getJsonData(request: Request): Promise<JsonRecord> {
 	if (request.headers.get("content-type") === "application/json") {
 		const body = await request.text();
 		const data = JSON.parse(body);
-		assertJsonRecord(data);
+		assertSchema(JsonRecordSchema, data);
 		return data;
 	}
 	return {};

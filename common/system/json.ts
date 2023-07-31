4b47359d2ebe9402f7351058cc0869f93c5d4684
Michael Grenier <michael@grenier.dev>
2023-07-30 22:11:37 -0400

    chore(schema): revert json validation

--- common/system/json.ts
+++ common/system/json.ts
@@ -1,25 +1,60 @@
-import { type Schema } from "../schema/types.ts";
-import * as s from "../schema/schema.ts";
-
-// Type definitions are nessary because TypeScript can't infer recursive
-// types yet.
-export type Json = JsonValue | JsonArray | JsonRecord;
+export type Json =
+	| JsonValue
+	| JsonArray
+	| JsonObject;
 export type JsonValue = string | number | boolean | null;
 export type JsonArray = Array<Json>;
-export type JsonRecord = { [key: string]: Json };
+export type JsonObject = { [key: string]: Json };
 
-export const JsonValueSchema: Schema<"union", JsonValue> = s.union([
-	s.string(),
-	s.number(),
-	s.boolean(),
-	s.nill(),
-]);
-export const JsonArraySchema: Schema<"array", JsonArray> = s.lazy(() =>
-	s.array(JsonSchema)
-);
-export const JsonRecordSchema: Schema<"record", JsonRecord> = s.lazy(() =>
-	s.record(JsonSchema)
-);
-export const JsonSchema: Schema<"union", Json> = s.lazy(() =>
-	s.union([JsonValueSchema, JsonArraySchema, JsonRecordSchema])
-);
+export function isJsonValue(value?: unknown): value is JsonValue {
+	return !(typeof value !== "undefined") || value === null ||
+		typeof value === "string" || typeof value === "number" ||
+		typeof value === "boolean";
+}
+export function assertJsonValue(value?: unknown): asserts value is JsonValue {
+	if (!isJsonValue(value)) {
+		throw new InvalidJsonValueError();
+	}
+}
+export class InvalidJsonValueError extends Error {
+	name = "InvalidJsonValueError" as const;
+}
+
+export function isJsonArray(value?: unknown): value is JsonArray {
+	return !!value && Array.isArray(value) && value.every(isJson);
+}
+export function assertJsonArray(value?: unknown): asserts value is JsonArray {
+	if (!isJsonArray(value)) {
+		throw new InvalidJsonArrayError();
+	}
+}
+export class InvalidJsonArrayError extends Error {
+	name = "InvalidJsonArrayError" as const;
+}
+
+export function isJsonObject(value?: unknown): value is JsonObject {
+	return !!value && typeof value === "object" &&
+		Object.values(value).every(isJson);
+}
+export function assertJsonObject(value?: unknown): asserts value is JsonObject {
+	if (!isJsonObject(value)) {
+		throw new InvalidJsonObjectError();
+	}
+}
+export class InvalidJsonObjectError extends Error {
+	name = "InvalidJsonObjectError" as const;
+}
+
+export function isJson(value?: unknown): value is Json {
+	return isJsonValue(value) || isJsonArray(value) || isJsonObject(value);
+}
+
+export function assertJson(value?: unknown): asserts value is Json {
+	if (!isJson(value)) {
+		throw new InvalidJsonError();
+	}
+}
+
+export class InvalidJsonError extends Error {
+	name = "InvalidJsonError" as const;
+}
--- server/api/get_json_data.ts
+++ server/api/get_json_data.ts
@@ -1,11 +1,10 @@
-import { assertSchema } from "../../common/schema/types.ts";
-import { type JsonRecord, JsonRecordSchema } from "../../common/system/json.ts";
+import { assertJsonObject, type JsonObject } from "../../common/system/json.ts";
 
-export async function getJsonData(request: Request): Promise<JsonRecord> {
+export async function getJsonData(request: Request): Promise<JsonObject> {
 	if (request.headers.get("content-type") === "application/json") {
 		const body = await request.text();
 		const data = JSON.parse(body);
-		assertSchema(JsonRecordSchema, data);
+		assertJsonObject(data);
 		return data;
 	}
 	return {};

fd58460b75eb73c3c59ed4f7883e30c3a0672704
Michael Grenier <michael@grenier.dev>
2024-01-10 22:24:09 -0500

    chore: more work on openapi generation

--- examples/hello-world/main.ts
+++ examples/hello-world/main.ts
@@ -9,34 +9,53 @@ const { publicKey, privateKey } = await generateKeyPair("PS512");
 const app = baseless()
 	.get(
 		"/hello/{world}",
-		(_req, { params }) =>
-			new Response(`Hello, ${decodeURIComponent(params.world)}!`),
+		(_req, { params, query }) =>
+			new Response(
+				`Hello, ${decodeURIComponent(params.world)}${query.exclamation ?? "!"}`,
+			),
 		{
 			summary: "Hello World",
 			description: "Says hello to the world.",
+			params: t.Object(
+				{ world: t.Describe("The placeholder", t.String()) },
+				["world"],
+			),
+			query: t.Object(
+				{
+					exclamation: t.Referenceable(
+						"ExclamationMark",
+						t.Describe("The exclamation mark", t.String()),
+					),
+				},
+			),
 			response: {
 				200: {
 					description: "La salutation",
 					content: {
 						"text/plain": {
-							schema: t.String(),
+							schema: t.Describe("La salutation", t.String()),
 						},
 					},
 				},
 			},
 		},
 	)
-	.use(auth({
-		keys: { publicKey, privateKey, algo: "PS512" },
-		salt: autoid() as string,
-		ceremonyComponent: c.sequence(),
-	}))
+	// .use(
+	// 	"/api/v1/auth",
+	// 	auth({
+	// 		keys: { publicKey, privateKey, algo: "PS512" },
+	// 		salt: autoid() as string,
+	// 		ceremonyComponent: c.sequence(),
+	// 	}),
+	// )
 	.use(openapi({
 		title: "Hello World Documentation",
 		version: "0.0.0-0",
 	}));
 
 const handle = await app.build();
+
+type routes = keyof Awaited<ReturnType<typeof app.getRoutes>>;
 
 const abortController = new AbortController();
 
--- server/openapi.ts
+++ server/openapi.ts
@@ -1,24 +1,18 @@
-import type { Definition, Method, Routes } from "../common/router/types.ts";
+import type { Definition, Routes } from "../common/router/types.ts";
 import { isObjectSchema, type Schema } from "../common/schema/types.ts";
 import { type BaselessContext, Router, t } from "./baseless.ts";
 import type { OpenAPIV3 } from "https://esm.sh/openapi-types@12.1.3";
 
+// deno-lint-ignore explicit-function-return-type
 export default function openapi(
 	info: OpenAPIV3.InfoObject,
 	servers: OpenAPIV3.ServerObject[] = [],
-): (routes: Routes) => Router<[BaselessContext]> {
-	return (routes: Routes) => {
-		const document: OpenAPIV3.Document = {
-			...transformRoutesToOpenAPIV3Document(routes),
-			openapi: "3.0.3",
-			info,
-			servers,
-		};
-		const json = JSON.stringify(document);
-
-		return new Router<[BaselessContext]>()
-			.get("/openapi", (_req, _input, _ctx) => {
-				const html = `<!DOCTYPE html>
+) {
+	return async (routes: Routes) => {
+		const app = new Router<[BaselessContext]>()
+			.get("/openapi.json", (req, _input, _ctx) => {
+				if (req.headers.get("accept")?.includes("text/html")) {
+					const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
@@ -26,22 +20,67 @@ export default function openapi(
 	<title>${info.title}</title>
 	<meta name="description" content="${info.description}" />
 	<meta name="og:description" content="${info.description}" />
-    <style>body { margin: 0 }</style>
+	<style>body { margin: 0 }</style>
 </head>
 <body>
 	<script id="api-reference" data-url="/openapi.json"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
 </body>
 </html>`;
-				return new Response(html, {
-					headers: { "content-type": "text/html; charset=utf-8" },
-				});
-			})
-			.get("/openapi.json", (_req, _input) => {
+					return new Response(html, {
+						headers: { "content-type": "text/html; charset=utf-8" },
+					});
+				}
 				return new Response(json, {
 					headers: { "content-type": "application/json" },
 				});
+			}, {
+				summary: `OpenAPI Documentation`,
+				description: `The OpenAPI documentation for this API`,
+				headers: t.Object({
+					Accept: t.Default(
+						"application/json",
+						t.Example(
+							"text/html",
+							t.Describe(
+								"The content type of the response",
+								t.String(),
+							),
+						),
+					),
+				}),
+				response: {
+					200: {
+						description: "The OpenAPI documentation for this API",
+						content: {
+							"text/html": {
+								schema: t.Example(
+									`<!DOCTYPE html>\n<html lang="en">\n\t...\n</html>`,
+									t.String(),
+								),
+							},
+							"application/json": {
+								schema: t.Example(
+									`{\n\t"openapi":"3.1.0",\n\t...\n}`,
+									t.String(),
+								),
+							},
+						},
+					},
+				},
 			});
+
+		routes = { ...routes, ...await app.getRoutes() };
+
+		const document: OpenAPIV3.Document = {
+			...transformRoutesToOpenAPIV3Document(routes),
+			openapi: "3.1.0",
+			info,
+			servers,
+		};
+		const json = JSON.stringify(document);
+
+		return app;
 	};
 }
 
@@ -50,25 +89,28 @@ function transformRoutesToOpenAPIV3Document(
 ): Pick<OpenAPIV3.Document, "components" | "paths"> {
 	const components = Object.entries(routes).reduce(
 		(components, [_path, operations]) => {
-			for (const [_method, { definition }] of Object.entries(operations)) {
-				for (
-					const key of [
-						"params",
-						"body",
-						"query",
-						"response",
-					] as (keyof Definition)[]
-				) {
-					const schema = definition[key] as Schema;
-					if (
-						schema && isObjectSchema(schema) &&
-						"$id" in schema && schema.$id
-					) {
-						components.schemas ??= {};
-						components.schemas[schema.$id] = schema as any;
-					}
-				}
-			}
+			// TODO: Make it recursive and search for all $id in schemas
+
+			// for (const [_method, { definition }] of Object.entries(operations)) {
+			// 	for (
+			// 		const key of [
+			// 			"params",
+			// 			"headers",
+			// 			"body",
+			// 			"query",
+			// 			"response",
+			// 		] as (keyof Definition)[]
+			// 	) {
+			// 		const schema = definition[key] as Schema;
+			// 		if (
+			// 			schema && isObjectSchema(schema) &&
+			// 			"$id" in schema && schema.$id
+			// 		) {
+			// 			components.schemas ??= {};
+			// 			components.schemas[schema.$id] = schema as any;
+			// 		}
+			// 	}
+			// }
 			return components;
 		},
 		{} as OpenAPIV3.ComponentsObject,
@@ -86,6 +128,11 @@ function transformRoutesToOpenAPIV3Document(
 						parameters: [
 							...schemaToParameterObject(components, "path", definition.params),
 							...schemaToParameterObject(components, "query", definition.query),
+							...schemaToParameterObject(
+								components,
+								"header",
+								definition.headers,
+							),
 						],
 						...(definition.body
 							? {
@@ -114,7 +161,7 @@ function schemaToRequestBody(
 	components: OpenAPIV3.ComponentsObject,
 	schema: Schema,
 ): OpenAPIV3.ReferenceObject | OpenAPIV3.RequestBodyObject {
-	const s = schema.$id && schema.$id in components
+	const s = schema.$id && components.schemas && schema.$id in components.schemas
 		? { $ref: `#/components/${schema.$id}` }
 		: schema as OpenAPIV3.SchemaObject;
 	return {
@@ -141,9 +188,10 @@ function schemaToParameterObject(
 	if (isObjectSchema(schema)) {
 		const objSchema = schema;
 		return Object.entries(schema.properties).map(([name, schema]) => {
-			const s = schema.$id && schema.$id in components
-				? { $ref: `#/components/${schema.$id}` }
-				: schema as OpenAPIV3.SchemaObject;
+			const s =
+				schema.$id && components.schemas && schema.$id in components.schemas
+					? { $ref: `#/components/${schema.$id}` }
+					: schema as OpenAPIV3.SchemaObject;
 			return {
 				in: location,
 				name,

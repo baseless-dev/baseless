4684cede22e8ef7a42069143a4ee0c250b135e88
Michael Grenier <michael@grenier.dev>
2024-02-18 21:59:08 -0500

    chore(doc): hello-world no longer use mock server

--- examples/hello-world/main.ts
+++ examples/hello-world/main.ts
@@ -3,49 +3,80 @@ import {
 	LogLevel,
 	setGlobalLogHandler,
 } from "../../lib/logger.ts";
+import { Router } from "../../lib/router/router.ts";
 import openapiPlugin from "../../plugins/openapi/mod.ts";
-import EmailAuthentificationComponent from "../../providers/auth-email/mod.ts";
-import OTPLoggerAuthentificationComponent from "../../providers/auth-otp-logger/mod.ts";
+import { MemoryAssetProvider } from "../../providers/asset-memory/mod.ts";
+import TOTPAuthentificationProvider from "../../providers/auth-totp/mod.ts";
+import OTPLoggerAuthentificationProvider from "../../providers/auth-otp-logger/mod.ts";
+import EmailAuthentificationProvider from "../../providers/auth-email/mod.ts";
+import PasswordAuthentificationProvider from "../../providers/auth-password/mod.ts";
+import { MemoryCounterProvider } from "../../providers/counter-memory/mod.ts";
+import { MemoryDocumentProvider } from "../../providers/document-memory/mod.ts";
+import { DocumentIdentityProvider } from "../../providers/identity-document/mod.ts";
+import { MemoryKVProvider } from "../../providers/kv-memory/mod.ts";
 import { LoggerMessageProvider } from "../../providers/message-logger/mod.ts";
-import { mock, t } from "../../server/mock.ts";
+import { KVSessionProvider } from "../../providers/session-kv/mod.ts";
+import authenticationPlugin from "../../plugins/authentication/mod.ts";
+import registrationPlugin from "../../plugins/registration/mod.ts";
+import { generateKeyPair, t } from "../../deps.ts";
+import { oneOf, sequence } from "../../lib/authentication/types.ts";
 
 setGlobalLogHandler(createConsoleLogHandler(LogLevel.DEBUG));
 
-const {
-	router,
-} = await mock(async ({
-	providers: { identity, kv },
-	components: { password, sequence, oneOf },
-}) => {
-	const message = new LoggerMessageProvider();
-	const email = new EmailAuthentificationComponent(
-		"email",
-		identity,
-		kv,
-		message,
-	);
-	const otp = new OTPLoggerAuthentificationComponent(
-		"otp",
-		{
-			digits: 6,
-		},
-		kv,
-	);
-	await identity.create({
-		displayName: "John Doe",
-	}, [{
-		id: "email",
-		...await email.configureIdentityComponent("john@test.local"),
-		confirmed: true,
-	}, {
-		id: "password",
-		...await password.configureIdentityComponent("123"),
-	}, {
-		id: "otp",
-		...await otp.configureIdentityComponent(""),
-	}]);
-	return {
-		auth: {
+const counter = new MemoryCounterProvider();
+const kv = new MemoryKVProvider();
+const document = new MemoryDocumentProvider();
+const asset = new MemoryAssetProvider();
+const identity = new DocumentIdentityProvider(new MemoryDocumentProvider());
+const session = new KVSessionProvider(new MemoryKVProvider());
+const message = new LoggerMessageProvider();
+const email = new EmailAuthentificationProvider(
+	"email",
+	identity,
+	kv,
+	message,
+);
+const password = new PasswordAuthentificationProvider(
+	"password",
+	"lesalt",
+);
+const otp = new OTPLoggerAuthentificationProvider(
+	"otp",
+	{
+		digits: 6,
+	},
+	kv,
+);
+const totp = new TOTPAuthentificationProvider("totp", {
+	digits: 6,
+	period: 60,
+});
+
+await identity.create({
+	displayName: "John Doe",
+}, [{
+	id: "email",
+	...await email.configureIdentityComponent("john@test.local"),
+	confirmed: true,
+}, {
+	id: "password",
+	...await password.configureIdentityComponent("123"),
+}, {
+	id: "otp",
+	...await otp.configureIdentityComponent(""),
+}]);
+const keys = { ...await generateKeyPair("PS512"), algo: "PS512" };
+
+const app = new Router()
+	.use(
+		"/api/authentication",
+		authenticationPlugin({
+			counter,
+			identity,
+			session,
+			kv,
+			keys,
+			salt: "lesalt",
 			providers: [
 				email,
 				password,
@@ -53,15 +84,36 @@ const {
 			],
 			ceremony: sequence(
 				email.toCeremonyComponent(),
-				password.toCeremonyComponent(),
 				otp.toCeremonyComponent(),
+				password.toCeremonyComponent(),
 			),
 			accessTokenTTL: 1000 * 60 * 60 * 10,
 			refreshTokenTTL: 1000 * 60 * 60 * 24 * 7,
-		},
-	};
-});
-const app = router
+		}),
+	)
+	.use(
+		"/api/registration",
+		registrationPlugin({
+			counter,
+			identity,
+			session,
+			keys,
+			providers: [
+				email,
+				password,
+				otp,
+			],
+			ceremony: sequence(
+				email.toCeremonyComponent(),
+				oneOf(
+					password.toCeremonyComponent(),
+					otp.toCeremonyComponent(),
+				),
+			),
+			accessTokenTTL: 1000 * 60 * 60 * 10,
+			refreshTokenTTL: 1000 * 60 * 60 * 24 * 7,
+		}),
+	)
 	.use(openapiPlugin({
 		info: {
 			title: "Hello World Documentation",

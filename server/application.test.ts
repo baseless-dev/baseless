32a0995b09a85f71aaaa7a53791a164539efe7c7
Michael Grenier <michael@grenier.dev>
2024-07-26 13:36:55 -0400

    chore(ci): application tests

--- server/application.test.ts
+++ server/application.test.ts
@@ -5,28 +5,15 @@ import { Type } from "@sinclair/typebox";
 import { ID } from "@baseless/core/id";
 
 Deno.test("Application", async (t) => {
-	await t.step("id", async () => {
+	await t.step("decorate", async () => {
 		const app = new Application()
 			.decorate(async () => {
 				return { foo: "bar" };
-			})
-			.rpc(["hello", "internal"], {
-				input: Type.String(),
-				output: Type.String(),
-				handler: async ({ input }) => {
-					return input;
-				},
-			})
-			.rpc(["hello", "world"], {
-				input: Type.String(),
-				output: Type.String(),
-				handler: async ({ input }) => {
-					return input;
-				},
-				async security() {
-					return "allow" as const;
-				},
-			})
+			});
+		assert(app.inspect().decorator.length === 1);
+	});
+	await t.step("rpc", async () => {
+		const app = new Application()
 			.rpc(["users", "{userId}", "kick"], {
 				input: Type.Void(),
 				output: Type.String(),
@@ -37,13 +24,27 @@ Deno.test("Application", async (t) => {
 					console.log({ userId });
 					return "allow" as const;
 				},
-			})
-			.emits(["some", "event"], {
+			});
+		const { rpc } = app.inspect();
+		assert(rpc.length === 1);
+		assertEquals(rpc[0].path, ["users", "{userId}", "kick"]);
+	});
+	await t.step("emits", async () => {
+		const app = new Application()
+			.emits(["foo", "bar"], {
 				payload: Type.Literal("a"),
 			})
-			.emits(["some", "other", "event"], {
-				payload: Type.Literal("b"),
-			})
+			.onEvent(["foo", "bar"], async ({ payload }) => {
+				console.log({ payload });
+			});
+		const { event, eventListeners } = app.inspect();
+		assert(event.length === 1);
+		assertEquals(event[0].path, ["foo", "bar"]);
+		assert(eventListeners.length === 1);
+		assertEquals(eventListeners[0].path, ["foo", "bar"]);
+	});
+	await t.step("document", async () => {
+		const app = new Application()
 			.document(["configuration"], {
 				schema: Type.Object({
 					foo: Type.String(),
@@ -52,23 +53,46 @@ Deno.test("Application", async (t) => {
 					return "read" as const;
 				},
 			})
+			.onDocumentSaving(["configuration"], async ({ document }) => {
+				console.log({ document });
+			})
+			.onDocumentSaved(["configuration"], async ({ document }) => {
+				console.log({ document });
+			})
+			.onDocumentDeleting(["configuration"], async ({ document }) => {
+				console.log({ document });
+			})
+			.onDocumentDeleted(["configuration"], async ({ document }) => {
+				console.log({ document });
+			});
+		const {
+			document,
+			documentSavingListeners,
+			documentSavedListeners,
+			documentDeletingListeners,
+			documentDeletedListeners,
+		} = app.inspect();
+		assert(document.length === 1);
+		assertEquals(document[0].path, ["configuration"]);
+		assert(documentSavingListeners.length === 1);
+		assertEquals(documentSavingListeners[0].path, ["configuration"]);
+		assert(documentSavedListeners.length === 1);
+		assertEquals(documentSavedListeners[0].path, ["configuration"]);
+		assert(documentDeletingListeners.length === 1);
+		assertEquals(documentDeletingListeners[0].path, ["configuration"]);
+		assert(documentDeletedListeners.length === 1);
+		assertEquals(documentDeletedListeners[0].path, ["configuration"]);
+	});
+	await t.step("collection", async () => {
+		const app = new Application()
 			.collection(["users"], {
 				schema: Type.Object({
 					userId: ID("usr_"),
 					username: Type.String(),
 				}),
-			})
-			.onEvent(["some", "other", "event"], async ({ payload }) => {
-				console.log({ payload });
-			})
-			.onDocumentSaved(["configuration"], async ({ document }) => {
-				console.log({ document });
-			})
-			.onDocumentSaving(
-				["configuration"],
-				async ({ atomic, document }) => {
-					console.log({ atomic, document });
-				},
-			);
+			});
+		const { collection } = app.inspect();
+		assert(collection.length === 1);
+		assertEquals(collection[0].path, ["users"]);
 	});
 });

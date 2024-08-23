e38533d3bfefeeab22a448aa1f6c362f62a9b1a4
Michael Grenier <michael@grenier.dev>
2024-08-22 22:10:03 -0400

    chore: eventemitter.on returns disposable

--- core/eventemitter.test.ts
+++ core/eventemitter.test.ts
@@ -9,14 +9,14 @@ Deno.test("EventEmitter", async (t) => {
 	await t.step("listen & emits", async () => {
 		const emitter = new EventEmitter().register<"foo", [foo: string]>();
 		const stack: string[] = [];
-		const unlistener = emitter.on("foo", (foo) => {
+		using listener = emitter.on("foo", (foo) => {
 			stack.push(foo);
 		});
 		await emitter.emit("foo", "bar1");
 		assertEquals(stack, ["bar1"]);
 		await emitter.emit("foo", "bar2");
 		assertEquals(stack, ["bar1", "bar2"]);
-		unlistener();
+		listener[Symbol.dispose]();
 		await emitter.emit("foo", "bar3");
 		assertEquals(stack, ["bar1", "bar2"]);
 	});
